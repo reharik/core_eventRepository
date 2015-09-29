3760293302fc6746109571303980ea2ec820f185
Raif Harik <reharik@gmail.com>
2015-09-28 19:25:52 -0500

    fixing up registry

--- registry.js
+++ registry.js
@@ -5,10 +5,12 @@ var dagon = require('dagon');
 
 module.exports = function(_options) {
     var options = _options || {};
-    var container = dagon(options.dagon);
+    var container = dagon(options.dagon || {});
     return new container(x=>
         x.pathToRoot(__dirname)
             .requireDirectoryRecursively('./src')
+            .for('eventmodels').instantiate(i=>i.asFunc())
+            .for('eventstore').instantiate(i=>i.asFunc().withParameters(options || {}))
             .for('bluebird').renameTo('Promise')
             .for('corelogger').renameTo('logger').instanciate(i=>i.asFunc().withParameters(options.logger || {}))
             .complete());
